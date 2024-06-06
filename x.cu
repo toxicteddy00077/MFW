a08b3806e31a627e0260937ef5bacbb25fe730c3
toxicteddy00077 <aryamaan.singh05@gmail.com>
2024-06-06 19:18:35 +0530

    RNG

---
+++ x.cu
@@ -0,0 +1,292 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <cuda_runtime.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <cmath>
+
+
+#define Cm cudaMalloc
+#define Cmc cudaMemcpy
+#define CmcHD cudaMemcpyHostToDevice
+#define CmcDH cudaMemcpyDeviceToHost
+
+
+__host__ void cpuBinConverter(int temprature,int* fin_bin)
+{
+    for(int i=31;i>=0;i--)
+    {
+        int x=((int)temprature>>i)&1;
+        fin_bin[31-i]=x;
+    }
+}
+
+__global__ void gpuBinConverter(int d_temprature,int* d_fin_bin)
+{
+    int tid=threadIdx.x+blockDim.x*blockIdx.x;
+    __shared__ int hold;
+    if(tid<32)
+    {
+        int hold=(int)d_temprature>>tid;
+        if(hold==1)
+        {
+            d_fin_bin[32-tid]=hold&1;
+        }
+        else d_fin_bin[32-tid]=0;
+    }
+}
+
+__host__ void cpuIntConverter(int* cells,unsigned int &ret,int size)
+{
+    int hold;
+    for(int i=0;i<size;i++)
+    {
+        hold+=pow(2,size-i-1)*cells[i];
+    }
+    ret=hold;
+}
+
+__global__ void gpuIntConverter(int* d_cells,int d_ret,int d_size)
+{
+    int tid=threadIdx.x+blockDim.x*blockIdx.x;
+    __shared__ int hold;
+    if(tid<d_size)
+    {
+        atomicAdd(&hold,pow(2,d_size-tid-1)*d_cells[tid]);
+    }
+    __syncthreads();
+    d_ret=hold;
+}
+
+__global__ void moveRight(int* arr,int* res,int delta)
+{
+    int value=arr[threadIdx.x];
+    value=__shfl_up_sync(0xFFFFFFFF,value,delta);
+    res[threadIdx.x]=value;
+    res[0]=0;
+}
+
+__global__ void hillisSteeleScan(int* arr,int *temp,int size)
+{
+
+    int tid=threadIdx.x+blockDim.x*blockIdx.x;
+    temp[tid]=arr[tid];
+    __syncthreads();
+    for(int stride=1;stride<=(size)/2;stride*=2)
+    {
+        if(tid>=stride)
+        {
+            temp[tid]=arr[tid]+arr[tid-stride];
+        }
+        else
+        {
+            temp[tid]=arr[tid];
+        }
+        __syncthreads();
+
+        arr[tid]=temp[tid];
+
+        __syncthreads();
+    }
+}   
+
+__host__ void cpuTransf(int* cells,int* out, int size)
+{
+    for(int i=0;i<size;i++)
+    {
+        out[i]=cells[i];
+    }
+}
+
+__global__ void gpuTransf(int* d_cells,int* d_out,int d_size)
+{
+    int tid=threadIdx.x+blockDim.x*blockIdx.x;
+    if(tid<d_size)
+    {
+        atomicAdd(&d_out[tid],d_cells[tid]);
+    }
+    __syncthreads();
+}
+
+__host__ void elementaryCellGenerate(int* cells,int* out,int size,int* rule)
+{
+    int p,q,r;
+    for(int i=1;i<size-1;i++)
+    {
+        p=4*cells[i-1];
+        q=2*cells[i];
+        r=cells[i+1];
+        out[i]=rule[7-(p+q+r)];
+    }
+}
+
+__global__ void gpuElementaryCellularAutoRule30(int* d_cells,int* d_out, int d_size,int* d_rule)
+{
+    int tid=threadIdx.x+blockDim.x*blockIdx.x;
+    __shared__ int p,q,r;
+    if(tid>0 && tid<d_size-1)
+    {
+        p=4*d_cells[tid-1];
+        q=2*d_cells[tid];
+        r=d_cells[tid+1];
+        d_out[tid]=d_rule[7-(p+q+r)];
+    }
+}   
+
+__host__ void cpuPrinter(int* cells,int size)
+{
+    for(int i=0;i<size;i++)
+    {
+        if(cells[i]==1) printf("1");
+        else printf("0");
+    }
+    printf("\n");
+}
+
+__global__ void gpuPrinter(int *d_cells,int d_size)
+{
+    int tid=threadIdx.x+blockDim.x*blockIdx.x;
+    if(tid<d_size)
+    {
+        if(d_cells[tid]==1) printf("1");
+        else printf("0");
+    }
+    printf("\n");
+}
+
+__host__ void cudaRNG(int seed,int size,int* rule)
+{
+    int *cells=(int*)malloc(size*sizeof(int));
+    int ret;
+    int *d_ret;
+    int *d_cells;
+    int *d_res;
+    int *d_out;
+    int *d_rule;
+
+    cpuBinConverter(seed,cells);
+
+    Cm((int**)&d_cells,size*sizeof(int));
+    Cm((int**)&d_res,size*sizeof(int));//temp
+    Cm((int**)&d_out,size*sizeof(int));//temp
+    Cm((int**)&d_ret,sizeof(int));
+    Cm((int**)&d_rule,8*sizeof(int));
+
+    Cmc(d_cells,cells,size*sizeof(int),CmcHD);
+    Cmc(d_rule,rule,8*sizeof(int),CmcHD);
+
+    int no_blocks=(size+31)/32;
+    for(int i=0;i<(size/2);i++)
+    {
+        gpuPrinter<<<1,1>>>(d_out,size);
+        gpuElementaryCellularAutoRule30<<<no_blocks,32>>>(d_cells,d_out,size,d_rule);
+
+        cudaDeviceSynchronize();
+
+        gpuTransf<<<no_blocks,32>>>(d_out,d_cells,size);
+    }
+    gpuIntConverter<<<1,1>>>(d_out,*d_ret,size);
+    Cmc(&ret,d_ret,sizeof(int),CmcDH);
+}
+
+int main(int argc, char** argv) 
+{
+    char path[256];
+    char filePath[512];
+    DIR *dir;
+    FILE *file;
+    float temperature;
+    float fin_temp;
+    struct dirent *ent;
+    
+    strcpy(path,"/sys/class/hwmon/");
+
+    dir=opendir(path);
+    if(dir==NULL)
+    {
+        perror("opendir");
+        return 1;
+    }
+
+    while ((ent = readdir(dir)) != NULL) {
+        if (strcmp(ent->d_name, ".") == 0 || strcmp(ent->d_name, "..") == 0)
+            continue;
+        snprintf(filePath,sizeof(filePath),"%s%s",path,ent->d_name);
+
+        DIR *subdir = opendir(filePath);
+        if (subdir==NULL) 
+        {
+            perror("opendir subdir");
+            continue;
+        }
+
+        struct dirent *subent;
+        while ((subent = readdir(subdir)) != NULL) {
+            if (strstr(subent->d_name, "temp") && strstr(subent->d_name, "_input")) {
+                snprintf(filePath,sizeof(filePath),"%s/%s/%s",path,ent->d_name,subent->d_name);
+
+                file = fopen(filePath,"r");
+                if (file==NULL) 
+                {
+                    perror("fopen");
+                    continue;
+                }
+
+                fscanf(file, "%f", &temperature);
+                printf("%2f \n",temperature/1000.0);
+                fin_temp=temperature/1000.0;
+                fclose(file);
+            }
+        }
+        closedir(subdir);
+    }
+    closedir(dir);
+
+    int *cells=(int*)malloc(64*sizeof(int));
+    cpuBinConverter((int)fin_temp,cells);
+    int rule[8]={0,0,0,1,1,1,1,0};
+    int newcells[8];
+    int out[64]={0};
+    unsigned int ret;
+
+    int *d_temp;
+    int *d_cells;
+    int *d_newcells;
+    
+    //cpu generation for refernce
+    for(int i=0;i<32;i++)
+    {
+        cpuPrinter(cells,64);
+        elementaryCellGenerate(cells,out,64,rule);
+        cpuTransf(out,cells,64);
+    }
+    cpuPrinter(out,64);
+    cpuIntConverter(out,ret,64);
+    printf("%d", ret);
+
+
+    Cm((int**)&d_cells,8*sizeof(int));
+    Cm((int**)&d_newcells,8*sizeof(int));
+    Cm((int**)&d_temp,4*sizeof(int));
+
+    Cmc(d_cells,cells,8*sizeof(int),CmcHD);
+    for(int i=0;i<2;i++)
+    {
+
+        Cmc(d_cells,cells,8*sizeof(int),CmcHD);
+        // gpuElementaryCellularAutoRule30<<<1,8>>>(d_cells,d_newcells,8);
+        cudaDeviceSynchronize();
+        Cmc(newcells,d_newcells,8*sizeof(int),CmcDH);
+        for (int i = 0; i < 8; i++) 
+        {
+            printf("%c ", newcells[i]);
+            cells[i]=newcells[i];
+        }
+        printf("\n");
+    }
+    printf("%d",(int)fin_temp);
+    return 0;
+}
